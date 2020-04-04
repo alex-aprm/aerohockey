d4d696a5062564964ed4740c64fffe3f6deeb208
Alex Paramonov <alex.aprm@gmail.com>
2020-04-05 00:33:00 +0300

    More physics

--- client/lib/src/app.dart
+++ client/lib/src/app.dart
@@ -3,12 +3,20 @@ part of app;
 class Application {
 
   CanvasElement field;
+
+  SpanElement debug = new SpanElement();
   Future init() async {
     api.getClient = () => new BrowserClient();
     document.body.children.clear();
     var engine = new Engine();
     engine.start();
     field = new CanvasElement(width: engine.field.width.round(), height: engine.field.height.round());
+    field.style.cursor = 'none';
+    field.onMouseMove.listen((me) {
+      engine.setPlayerPosition(engine.bluePlayer, me.client.x * engine.field.width / field.clientWidth,
+          me.client.y * engine.field.height / field.clientHeight);
+    });
+
     var rnd = new math.Random();
     var btn = new ButtonElement()..text = 'BOOM';
     btn.onClick.listen((_) {
@@ -18,14 +26,18 @@ class Application {
     });
     document.body.append(field);
     document.body.append(btn);
+    document.body.append(debug);
     new Timer.periodic(new Duration(milliseconds: 1), (_) => drawField(engine));
   }
 
   Future drawField(Engine engine) async {
+    debug.text = 'sx = ${engine.puck.spin}';
     var ctx = field.context2D;
+    ctx.setFillColorRgb(255, 255, 255);
+    ctx.fillRect(0,0, engine.field.width, engine.field.height);
     ctx.setFillColorRgb(240, 240, 240);
     ctx.fillRect(engine.field.border, engine.field.border, engine.field.width - 2 * engine.field.border, engine.field.height - 2 * engine.field.border);
-    ctx.setFillColorRgb(255, 0, 0);
+    ctx.setFillColorRgb(0, 255, 0);
     ctx.beginPath();
     ctx.ellipse(engine.puck.x, engine.puck.y, engine.puck.radius, engine.puck.radius, 0, 0, 2 * math.PI, false);
     ctx.fill();
@@ -33,6 +45,12 @@ class Application {
     ctx.moveTo(engine.puck.x, engine.puck.y);
     ctx.lineTo(engine.puck.x + math.cos(engine.puck.phi)*engine.puck.radius, engine.puck.y + math.sin(engine.puck.phi)*engine.puck.radius);
     ctx.stroke();
+
+    ctx.setFillColorRgb(0, 0, 255);
+    ctx.beginPath();
+    ctx.ellipse(engine.bluePlayer.x, engine.bluePlayer.y, engine.bluePlayer.radius, engine.bluePlayer.radius, 0, 0, 2 * math.PI, false);
+    ctx.fill();
+
   }
 
 }
--- shared/lib/src/engine.dart
+++ shared/lib/src/engine.dart
@@ -1,28 +1,32 @@
 part of shared;
 
 class Engine {
-  double friction = 0.2;
+  double friction = 0.4;
 
   double frame = 1000000.0;
 
   Stopwatch stopwatch;
   int lastProcessedTick = 0;
 
-  Puck redPlayer;
-  Puck bluePlayer;
+  PlayerPuck redPlayer;
+  PlayerPuck bluePlayer;
   Puck puck;
   Field field = new Field();
 
   void start() {
     stopwatch = new Stopwatch();
     stopwatch.start();
-    redPlayer = new Puck(50.0, 50.0);
-    puck = new Puck(30.0, 30.0);
-    puck.x = field.width / 2;
-    puck.y = field.height / 2;
+    bluePlayer = new PlayerPuck(50.0, 50.0, field.width / 2, field.height * 3 / 4);
+
+    puck = new Puck(30.0, 30.0, field.width / 2, field.height / 2);
     puck.spin = 2 * math.PI;
 
     processTick();
+  }
+
+  void setPlayerPosition(PlayerPuck player, double x, double y) {
+    player.desiredX = x;
+    player.desiredY = y;
   }
 
   Future processTick() async {
@@ -34,6 +38,7 @@ class Engine {
     lastProcessedTick = elapsedTotal;
 
     puck.process(elapsedTicks, this);
+    bluePlayer.process(elapsedTicks, this);
 
     Timer.run(processTick);
   }
@@ -52,13 +57,11 @@ class Puck {
     while (phi >= 2* math.PI )
       phi -= 2* math.PI;
 
-    var speed = math.sqrt(speedX * speedX + speedY * speedY);
-
     if (speedX < 0) {
       if (newX - radius <= field.border) {
         newX += 2*(field.border - (newX - radius));
         speedX = - speedX;
-        speedY += 0.5 * speed * spin * elapsedTicks / engine.frame;
+        speedY +=  speed * spin * elapsedTicks / engine.frame;
       }
     }
 
@@ -66,7 +69,7 @@ class Puck {
       if (newX + radius >= field.width - field.border) {
         newX -= 2*((newX + radius) - (field.width - field.border));
         speedX = - speedX;
-        speedY -= 0.5 * speed* spin * elapsedTicks / engine.frame;
+        speedY -=  speed* spin * elapsedTicks / engine.frame;
       }
     }
 
@@ -74,7 +77,7 @@ class Puck {
       if (newY - radius <= field.border) {
         newY += 2*(field.border - (newY - radius));
         speedY = - speedY;
-        speedX -= 0.5 * speed * spin * elapsedTicks / engine.frame;
+        speedX -= speed * spin * elapsedTicks / engine.frame;
       }
     }
 
@@ -82,7 +85,7 @@ class Puck {
       if (newY + radius >= field.height - field.border) {
         newY -= 2*((newY + radius) - (field.height - field.border));
         speedY = - speedY;
-        speedX += 0.5 * speed * spin * elapsedTicks / engine.frame;
+        speedX +=  speed * spin * elapsedTicks / engine.frame;
       }
     }
 
@@ -105,13 +108,64 @@ class Puck {
 
   double speedX = 0.0;
   double speedY = 0.0;
+
+  double get impulse => weight * speed;
+
+  double get speed => math.sqrt(speedX * speedX + speedY * speedY);
   double spin = 0.0;
 
   double weight;
   double size;
   double get radius => size / 2;
 
-  Puck(this.size, this.weight);
+  Puck(this.size, this.weight, this.x, this.y);
+}
+
+
+class PlayerPuck extends Puck {
+  PlayerPuck(double size, double weight, this.desiredX, this.desiredY): super(size, weight, desiredX, desiredY);
+
+  double desiredX;
+  double desiredY;
+
+  void process(int elapsedTicks, Engine engine) {
+    desiredX = desiredX.clamp(engine.field.border + radius, engine.field.width - engine.field.border - radius);
+    desiredY = desiredY.clamp(engine.field.border + radius, engine.field.height - engine.field.border - radius);
+    if (desiredX != x)
+      speedX = (desiredX - x) * 100;
+    if (desiredY != y)
+      speedY = (desiredY - y) * 100;
+
+
+    var newX = x + speedX * elapsedTicks / engine.frame;
+    var newY = y + speedY * elapsedTicks / engine.frame;
+
+    var puck = engine.puck;
+    var distance = math.sqrt((puck.x - newX) * (puck.x - newX) + (puck.y - newY) * (puck.y - newY));
+    if (distance <= radius + puck.radius) {
+      var imp = (impulse > puck.impulse ? impulse : puck.impulse).clamp(0, 100000);
+      var newPuckSpeed = engine.friction * imp / puck.weight;
+      puck.speedX = puck.x - newX;
+      puck.speedY = puck.y - newY;
+      var sx = puck.speedX / puck.speed;
+      var sy = puck.speedY / puck.speed;
+
+      puck.speedX = sx * newPuckSpeed;
+      puck.speedY = sy * newPuckSpeed;
+
+      var angle = math.acos(speedX/speed) - math.acos(puck.speedX/puck.speed);
+
+      puck.spin = angle*puck.speed/50;
+
+    }
+    else{
+      x = newX;
+      y = newY;
+    }
+
+  }
+
+
 }
 
 class Field {
