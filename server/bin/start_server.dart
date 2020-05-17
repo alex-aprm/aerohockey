3951859f93043249e2fa313f0a2a7dc4499acd92
Alex Paramonov <alex.aprm@gmail.com>
2020-05-17 21:28:58 +0300

    Start server as a process

--- server/bin/start_server.dart
+++ server/bin/start_server.dart
@@ -1,7 +1,14 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:server/server.dart';
 
-Future main() async {
+Future main(List<String> args) async {
+  if (args.isNotEmpty) {
+    if (args[0] == 'start') {
+      await Process.start('dart', ['start_server.dart']);
+      exit(0);
+    }
+  }
   var server = new Server();
   await server.start();
 }
